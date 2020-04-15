fb21baa6320d9bbfd2fbed48953fa1b42ac51973
sunnycucumber <guigui.nevoux@gmeil.com>
2020-04-15 20:01:52 +0200

    TD4

--- app/src/main/java/com/vogella/android/recyclerview/MainActivity.java
+++ app/src/main/java/com/vogella/android/recyclerview/MainActivity.java
@@ -2,20 +2,38 @@ package com.vogella.android.recyclerview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity <recyclerView> extends Activity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
+public class MainActivity <recyclerView> extends Activity {
+    private static final String BASE_URL = "https://pokeapi.co/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showList();
+        makeApiCall();
+    }
+
+
+
+    private void showList() {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         // use this setting to
         // improve performance if you know that changes
@@ -29,8 +47,48 @@ public class MainActivity <recyclerView> extends Activity {
         for (int i = 0; i < 100; i++) {
             input.add("Test" + i);
         }
-        RecyclerView.Adapter mAdapter = new Adapter(input);
+        RecyclerView.Adapter<Adapter.ViewHolder> mAdapter = new Adapter(input);
         recyclerView.setAdapter(mAdapter);
     }
+
+
+    private void makeApiCall(){
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+
+            PokeApi pokeApi =  retrofit.create(PokeApi.class);
+
+            Call<RestPokemonResponse> call = pokeApi.getPokemonResponse();
+            call.enqueue(new Callback<RestPokemonResponse>() {
+                @Override
+                public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response) {
+                    if(response.isSuccessful() && response.body() != null) {
+                            List<Pokemon> pokemonList = response.body().getResults();
+                        Toast.makeText(getApplicationContext(), "Api SUCCESS", Toast.LENGTH_SHORT).show();
+
+                    } else{
+                        showError();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<RestPokemonResponse> call, Throwable t) {
+                    showError();
+                }
+            });
+
+        }
+        private void showError(){
+            Toast.makeText(getApplicationContext(), "Api ERROR", Toast.LENGTH_SHORT).show();
+        }
+
+
+
 
 }
---
+++ app/src/main/java/com/vogella/android/recyclerview/Pokemon.java
@@ -0,0 +1,14 @@
+package com.vogella.android.recyclerview;
+
+public class Pokemon {
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
